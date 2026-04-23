482a8ac99fc70620f96f6e9d7ab8fa4b5dc6d3aa
aamiot99-cmd <aamiot99-cmd@users.noreply.github.com>
2026-04-23 15:13:51 +0200

    Auto-save: 2026-04-23 15:13:51

--- src/widgets/news/renderers/sharepoint.tsx
+++ src/widgets/news/renderers/sharepoint.tsx
@@ -19,7 +19,7 @@ function getArticles(company: string): { featured: Article; secondary: Article[]
       author: 'Théo Dumont',
       time: 'il y a 6 minutes',
       views: '2 vues',
-      image: 'https://images.unsplash.com/photo-1557683316-973673baf926?w=600&q=80',
+      image: '/news/pexels-brunogobofoto-3861712.jpg',
     },
     secondary: [
       {
